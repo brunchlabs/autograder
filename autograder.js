6ffd3cd8ca89d53e81a184635ea7ae0f730ab57a
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 15:28:51 -0400

    Add

--- autograder.js
+++ autograder.js
@@ -1,12 +1,9 @@
 const fs = require('fs');
-const core = require('@actions/core');
 
 try {
-  // const context = JSON.parse(fs.readFileSync('context.json', 'utf8'));
-  const report = core.getInput('report');
+  const report = JSON.parse(fs.readFile('report.json', 'utf8'));
 
-  console.log(report);
-  console.log(report);
+  console.log('here', report);
 } catch (error) {
   core.setFailed(`Action failed: ${error.message}`);
 }
