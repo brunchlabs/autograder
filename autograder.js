fe7039f442a1490312bdbff7ddd2f668999cfaa5
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 16:57:26 -0400

    remove install

--- autograder.js
+++ autograder.js
@@ -2,25 +2,7 @@ const core = require('@actions/core');
 
 try {
   console.log('here', process.env);
-  // const repo = core.getInput('repo');
-  const student = core.getInput('student');
-  const studentId = core.getInput('studentId');
-  const organization = core.getInput('organization');
-  const organizationId = core.getInput('organizationId');
-  const report = core.getInput('report');
-  const branch = core.getInput('branch');
-
-  const repo = '${{ inputs.repo }}';
-
-  console.log('repo', repo);
-  console.log('student', student);
-  console.log('repo', repo);
-  console.log('student', student);
-  console.log('studentId', studentId);
-  console.log('organization', organization);
-  console.log('organizationId', organizationId);
-  console.log('report', report);
-  console.log('branch', branch);
+  const repo = core.getInput('repo');
 } catch (error) {
   core.setFailed(`Action failed: ${error.message}`);
 }
