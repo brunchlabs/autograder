a1168381a77192e7d657f621e8728e35487acfe8
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 14:45:40 -0400

    add core

--- autograder.js
+++ autograder.js
@@ -5,18 +5,18 @@ const core = require('@actions/core');
 try {
   // Parse the GitHub context passed as a string
   console.log('before', core.getInput('context'));
-  const githubContext = JSON.parse(core.getInput('context'));
+  // const githubContext = JSON.parse(core.getInput('context'));
 
-  console.log(githubContext);
+  // console.log(githubContext);
 
-  // Now you can access all properties of the GitHub context
-  const { repository, actor, ref, repository_owner } = githubContext;
+  // // Now you can access all properties of the GitHub context
+  // const { repository, actor, ref, repository_owner } = githubContext;
 
-  // Use these values as needed for your autograder logic
-  console.log(`Repository: ${repository}`);
-  console.log(`Actor: ${actor}`);
-  console.log(`Ref: ${ref}`);
-  console.log(`Repository Owner: ${repository_owner}`);
+  // // Use these values as needed for your autograder logic
+  // console.log(`Repository: ${repository}`);
+  // console.log(`Actor: ${actor}`);
+  // console.log(`Ref: ${ref}`);
+  // console.log(`Repository Owner: ${repository_owner}`);
 
   // Continue with your autograder logic...
 } catch (error) {
