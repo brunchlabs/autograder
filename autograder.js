189a48fce6599421a50a0ddcded8550cbd7e4ad6
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 17:55:37 -0400

    remove install

--- autograder.js
+++ autograder.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const fs = require('fs');
 
 try {
   const repo = process.env.REPO;
@@ -6,8 +7,9 @@ try {
   const studentId = process.env.STUDENT_ID;
   const organization = process.env.ORGANIZATION;
   const organizationId = process.env.ORGANIZATION_ID;
-  const report = process.env.REPORT;
   const branch = process.env.BRANCH;
+
+  const report = fs.readFileSync('report.json', 'utf8');
 
   console.log('report', report);
 } catch (error) {
