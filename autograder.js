449ad151fcfee9ccb066d0de807416a177d06eda
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 15:00:41 -0400

    add core

--- autograder.js
+++ autograder.js
@@ -3,13 +3,10 @@ const fs = require('fs');
 const core = require('@actions/core');
 
 try {
-  const path = process.env.GITHUB_CONTEXT_FILE || 'context.json';
-  const raw = fs.readFileSync(path, 'utf8');
+  const raw = fs.readFileSync('context.json', 'utf8');
+  const context = JSON.parse(raw);
 
-  console.log('raw', raw);
-  // const context = JSON.parse(raw);
-
-  // console.log(context);
+  console.log(context);
 } catch (error) {
   core.setFailed(`Action failed: ${error.message}`);
 }
