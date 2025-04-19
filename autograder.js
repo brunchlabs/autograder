3a6f780d00937f687cd9b5dbc271d22761e51c0b
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-19 15:36:55 -0400

    Add

--- autograder.js
+++ autograder.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 
 try {
+  const repo = core.getInput('repo');
   const student = core.getInput('student');
   const studentId = core.getInput('student_id');
   const organization = core.getInput('organization');
@@ -8,6 +9,7 @@ try {
   const report = core.getInput('report');
   const branch = core.getInput('branch');
 
+  console.log('repo', repo);
   console.log('student', student);
   console.log('studentId', studentId);
   console.log('organization', organization);
