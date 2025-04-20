166aa84b6c8ea7bd68727526cb5a1e43a751700f
Pape Sow Traoré <papisline2222@gmail.com>
2025-04-20 13:27:12 -0400

    add url

--- autograder.js
+++ autograder.js
@@ -3,21 +3,15 @@ const core = require('@actions/core');
 
 function run() {
   try {
-    const repo = process.env.REPO;
     const repositoryId = process.env.REPOSITORY_ID;
-    const student = process.env.STUDENT;
     const studentId = process.env.STUDENT_ID;
-    const organization = process.env.ORGANIZATION;
     const organizationId = process.env.ORGANIZATION_ID;
     const branch = process.env.BRANCH;
     const report = JSON.parse(process.env.REPORT || '{}');
 
     const data = {
-      repo,
       repositoryId,
-      student,
       studentId,
-      organization,
       organizationId,
       branch,
       report,
